f7c2d0d3e4a5d0615b8baf84d4fcc277f74dcd86
LimitToInfinity <21stLimitless@gmail.com>
2020-06-15 14:30:57 -0600

    Removes other values being sent upon login.

--- cats_api/app/controllers/authentication_controller.rb
+++ cats_api/app/controllers/authentication_controller.rb
@@ -10,11 +10,7 @@ class AuthenticationController < ApplicationController
                 secret = Rails.application.secrets.secret_key_base
                 token = JWT.encode(payload, secret)
 
-                render json: {
-                    token: token,
-                    cats: @user.cats,
-                    user_id: @user.id
-                }
+                render json: { token: token }
             else
                 render json: {message: "nice try asshole!!!"}, status: :unauthorized
             end
